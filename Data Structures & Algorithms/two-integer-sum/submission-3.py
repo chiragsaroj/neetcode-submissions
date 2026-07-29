1ed4a627452583b078710ac1b61e383e6eacc33c
Chirag Saroj <68062665+chiragsaroj@users.noreply.github.com>
2026-07-29 12:16:54 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = dict()
+
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+
+            if compliment in map:
+                return [map[compliment], i]
+
+            map[nums[i]] = i
+
+        return [-1, -1]
